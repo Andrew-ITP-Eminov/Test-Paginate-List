d57a0e1bcc2db701896220199f2ac579abc95018
Andrew <you@example.com>
2020-07-15 12:47:37 +0300

    created testing data

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import {applyMiddleware, createStore} from "redux";
+import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {rootReducer} from "./reducers/index";
+import IndexSaga from "./middleware/saga/indexSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer,composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+));
+sagaMiddleware.run(IndexSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
